79155ae18b6e9da31204168a46219f6eec3ff0d3
Jordan Lesaca <lesaca.j@northeastern.edu>
2026-04-22 03:17:52 -0700

    more styling changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
 
 let current = 1;
-const total = cards.length;
+const total = cards.length; 
 
 function getIndex(offset) {
   return (current + offset + total) % total;
@@ -13,7 +13,6 @@ function updateCarousel() {
   cards.forEach((card) => {
     card.className = 'carousel-card hidden';
   });
-
   cards[getIndex(-1)].className = 'carousel-card back-left';
   cards[current].className = 'carousel-card center';
   cards[getIndex(1)].className = 'carousel-card back-right';
